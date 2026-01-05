99f9c0f71edd41ccdf7ff1875d2264ca4ed4d2ae
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2026-01-05 19:22:17 +0000

    image screen

---
+++ code.py
@@ -0,0 +1,27 @@
+import stage
+import ugame
+
+
+def game_scene():
+    # this function is for the main game game_scene
+
+    # image banks for circuitpython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    # sets the background to image 0 in the bank
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    # create a stage for the game
+    game = stage.Stage(ugame.display, 60)
+    # set the layers of all sprites
+    game.layers = [background]
+    # renders the sprites
+    game.render_block()
+
+    # repeats game forever
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    game_scene()
