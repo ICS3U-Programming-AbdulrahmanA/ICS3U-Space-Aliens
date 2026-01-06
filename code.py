f4580cbfe6e824831596d89c2232b51fbff2bc3c
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2026-01-06 18:25:43 +0000

    03

--- code.py
+++ code.py
@@ -7,14 +7,18 @@ def game_scene():
 
     # image banks for circuitpython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # sets the background to image 0 in the bank
     background = stage.Grid(image_bank_background, 10, 8)
 
+    # create a sprite
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
     # create a stage for the game
     game = stage.Stage(ugame.display, 60)
     # set the layers of all sprites
-    game.layers = [background]
+    game.layers = [ship] + [background]
     # renders the sprites
     game.render_block()
 
