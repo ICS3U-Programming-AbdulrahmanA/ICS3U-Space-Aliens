df2e4af597c36bfca8e22fae3bd99dc4c0ffb734
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2026-01-12 18:40:50 +0000

    09

--- code.py
+++ code.py
@@ -165,11 +165,17 @@ def game_scene():
         16,
     )
 
+    lasers = []
+    for laser_number in range(constants.TOTAL_NUMBER_OF_LASERS):
+        a_single_laser = stage.Sprite(image_bank_sprites, 10, constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+        lasers.append(a_single_laser)
+    
+
     # create a stage for the background to show up on
     # and set the frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
     # set the layers of all sprites, items show up in order
-    game.layers = [ship] + [alien] + [background]
+    game.layers = lasers + [ship] + [alien] + [background]
     # render all sprites
     # most likely you will only render the background once per game scene
     game.render_block()
@@ -217,10 +223,20 @@ def game_scene():
         # update game logic
         # play sound if A was just button_just_pressed
         if a_button == constants.button_state["button_just_pressed"]:
-            sound.play(pew_sound)
+            for laser_number in range(len(lasers)):
+                if lasers[laser_number].x < 0:
+                    lasers[laser_number].move(ship.x, ship.y)
+                    sound.play(pew_sound)
+                    break
+
+        for laser_number in range(len(lasers)):
+                if lasers[laser_number].x > 0:
+                    lasers[laser_number].move(lasers[laser_number].x, lasers[laser_number].y - constants.LASER_SPEED)
+                    if lasers[laser_number].y < constants.OFF_SCREEN_Y:
+                        lasers[laser_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
 
         # render sprites
-        game.render_sprites([ship] + [alien])
+        game.render_sprites(lasers + [ship] + [alien])
         game.tick()
 
 
