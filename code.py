eb2dfe58a8e186197bd109bb49e646c254d54983
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2026-01-13 19:08:55 +0000

    12

--- code.py
+++ code.py
@@ -75,7 +75,6 @@ def splash_scene():
         game.tick()
 
 
-
 def menu_scene():
     # this function is the main menu game_scene
 
@@ -130,19 +129,22 @@ def game_scene():
         for alien in aliens:
             if alien.y < 0:
                 alien.move(
-                    random.randint(
-                        0,
-                        constants.SCREEN_X - constants.SPRITE_SIZE
-                    ),
-                    0
+                    random.randint(0, constants.SCREEN_X - constants.SPRITE_SIZE), 0
                 )
                 break
-
 
     # this function is the main game game_scene
 
     score = 0
-    
+
+    score_text = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
+    score_text.clear()
+    score_text.cursor(0, 0)
+    score_text.move(1, 1)
+    score_text.text("Score: {}".format(score))
+
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
@@ -183,8 +185,10 @@ def game_scene():
 
     aliens = []
     for alien_number in range(constants.TOTAL_NUMBER_OF_ALIENS):
-        a_single_alien = stage.Sprite(image_bank_sprites, 9, constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
-        aliens.append(a_single_alien) 
+        a_single_alien = stage.Sprite(
+            image_bank_sprites, 9, constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+        )
+        aliens.append(a_single_alien)
     show_alien()
 
     alien_spawn_timer = 0
@@ -192,25 +196,22 @@ def game_scene():
     lasers = []
     for laser_number in range(constants.TOTAL_NUMBER_OF_LASERS):
         a_single_laser = stage.Sprite(
-            image_bank_sprites,
-            10,
-            constants.OFF_SCREEN_X,
-            constants.OFF_SCREEN_Y
+            image_bank_sprites, 10, constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
         )
         lasers.append(a_single_laser)
 
-
     lasers = []
     for laser_number in range(constants.TOTAL_NUMBER_OF_LASERS):
-        a_single_laser = stage.Sprite(image_bank_sprites, 10, constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+        a_single_laser = stage.Sprite(
+            image_bank_sprites, 10, constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+        )
         lasers.append(a_single_laser)
-    
 
     # create a stage for the background to show up on
     # and set the frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
     # set the layers of all sprites, items show up in order
-    game.layers = aliens + lasers + [ship] + [background]
+    game.layers = [score_text] + aliens + lasers + [ship] + [background]
     # render all sprites
     # most likely you will only render the background once per game scene
     game.render_block()
@@ -221,10 +222,9 @@ def game_scene():
         keys = ugame.buttons.get_pressed()
 
         alien_spawn_timer += 1
-        if alien_spawn_timer > 60:   # about 1 second at 60 FPS
+        if alien_spawn_timer > 60:  # about 1 second at 60 FPS
             show_alien()
             alien_spawn_timer = 0
-
 
         # A button to fire
         if keys & ugame.K_O != 0:
@@ -271,44 +271,69 @@ def game_scene():
                     break
 
         for laser_number in range(len(lasers)):
-                if lasers[laser_number].x > 0:
-                    lasers[laser_number].move(lasers[laser_number].x, lasers[laser_number].y - constants.LASER_SPEED)
-                    if lasers[laser_number].y < constants.OFF_SCREEN_Y:
-                        lasers[laser_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+            if lasers[laser_number].x > 0:
+                lasers[laser_number].move(
+                    lasers[laser_number].x,
+                    lasers[laser_number].y - constants.LASER_SPEED,
+                )
+                if lasers[laser_number].y < constants.OFF_SCREEN_Y:
+                    lasers[laser_number].move(
+                        constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                    )
 
         for alien_number in range(len(aliens)):
-                if aliens[alien_number].x > 0:
-                    aliens[alien_number].move(aliens[alien_number].x, aliens[alien_number].y + constants.ALIEN_SPEED)
-                    if aliens[alien_number].y > constants.SCREEN_Y:
-                        aliens[alien_number].move(
-                            constants.OFF_SCREEN_X,
-                            constants.OFF_SCREEN_Y
+            if aliens[alien_number].x > 0:
+                aliens[alien_number].move(
+                    aliens[alien_number].x,
+                    aliens[alien_number].y + constants.ALIEN_SPEED,
                 )
-                show_alien()
+                if aliens[alien_number].y > constants.SCREEN_Y:
+                    aliens[alien_number].move(
+                        constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                    )
+                    show_alien()
+                    score = score - 1
+                    if score < 0:
+                        score = 0
+                    score_text.clear()
+                    score_text.cursor(0, 0)
+                    score_text.move(1, 1)
+                    score_text.text("Score: {}".format(score))
 
         for laser_number in range(len(lasers)):
             if lasers[laser_number].x > 0:
                 for alien_number in range(len(aliens)):
                     if aliens[alien_number].x > 0:
-                        if stage.collide(lasers[laser_number].x + 6, lasers[laser_number].y + 2,
-                                        lasers[laser_number].x + 11, lasers[laser_number].y + 12,
-                                        aliens[alien_number].x + 1, aliens[alien_number].y,
-                                        aliens[alien_number].x + 15, aliens[alien_number].y + 15):
-                                        aliens[alien_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
-                                        lasers[laser_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
-                                        sound.stop()
-                                        boom_sound = open("boom.wav", "rb")
-                                        sound.play(boom_sound)
-                                        show_alien()
-                                        show_alien()
-                                        score = score + 1
-
-
+                        if stage.collide(
+                            lasers[laser_number].x + 6,
+                            lasers[laser_number].y + 2,
+                            lasers[laser_number].x + 11,
+                            lasers[laser_number].y + 12,
+                            aliens[alien_number].x + 1,
+                            aliens[alien_number].y,
+                            aliens[alien_number].x + 15,
+                            aliens[alien_number].y + 15,
+                        ):
+                            aliens[alien_number].move(
+                                constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                            )
+                            lasers[laser_number].move(
+                                constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                            )
+                            sound.stop()
+                            boom_sound = open("boom.wav", "rb")
+                            sound.play(boom_sound)
+                            show_alien()
+                            show_alien()
+                            score = score + 1
+                            score_text.clear()
+                            score_text.cursor(0, 0)
+                            score_text.move(1, 1)
+                            score_text.text("Score: {}".format(score))
 
         # render sprites
         game.render_sprites(aliens + lasers + [ship])
         game.tick()
-
 
 
 if __name__ == "__main__":
